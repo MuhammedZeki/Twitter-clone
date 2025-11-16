8b53dbb44e51c32027112800cf1cb723e2ef22bc
MuhammedZeki <muhammed.codes@gmail.com>
2025-11-16 18:49:13 +0300

    Suggested fn added

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,11 +2,12 @@ import express from "express";
 import { getUserProfile } from "../controllers/user.controller.js";
 import { protectRoute } from "../middlewares/protectRoute.js";
 import { followUnfollowUser } from "../controllers/user.controller.js";
+import { getSuggestedUsers } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/profile/:username", protectRoute, getUserProfile);
 router.post("/follow/:id", protectRoute, followUnfollowUser);
-
+router.get("/suggested", protectRoute, getSuggestedUsers);
 
 export default router;
