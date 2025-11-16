6ad1753e7de7a8b7ff8220fc40beb4177f8aab41
MuhammedZeki <muhammed.codes@gmail.com>
2025-11-16 19:01:07 +0300

    updateUser fn added

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -3,11 +3,13 @@ import { getUserProfile } from "../controllers/user.controller.js";
 import { protectRoute } from "../middlewares/protectRoute.js";
 import { followUnfollowUser } from "../controllers/user.controller.js";
 import { getSuggestedUsers } from "../controllers/user.controller.js";
+import { updateUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/profile/:username", protectRoute, getUserProfile);
 router.post("/follow/:id", protectRoute, followUnfollowUser);
 router.get("/suggested", protectRoute, getSuggestedUsers);
+router.post("/update", protectRoute, updateUser);
 
 export default router;
