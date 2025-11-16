e4035bfb909cf50ab5c41244ab68199d54a025ff
MuhammedZeki <muhammed.codes@gmail.com>
2025-11-16 20:00:35 +0300

    deletePost fn added

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createPost } from "../controllers/post.controller.js";
+import { createPost, deletePost } from "../controllers/post.controller.js";
 import { protectRoute } from "../middlewares/protectRoute.js";
 const router = express.Router();
 
 router.post("/create", protectRoute, createPost);
+router.delete("/:id", protectRoute, deletePost);
 
 
 export default router;
