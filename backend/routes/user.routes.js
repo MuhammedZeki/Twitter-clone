641e6549d2d42573d294fca018a90ef6c7c4d3d5
MuhammedZeki <muhammed.codes@gmail.com>
2025-11-16 18:41:06 +0300

    followUnfollow fn added

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { getUserProfile } from "../controllers/user.controller.js";
 import { protectRoute } from "../middlewares/protectRoute.js";
+import { followUnfollowUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/profile/:username", protectRoute, getUserProfile);
-// router.get("/suggested", protectRoute, getSuggestedUsers);
-// router.post("/follow/:id", protectRoute, followUnfollowUser);
-// router.post("/update", protectRoute, updateUser);
+router.post("/follow/:id", protectRoute, followUnfollowUser);
+
 
 export default router;
