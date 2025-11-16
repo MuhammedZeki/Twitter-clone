1cba13ca04e247b353c9c1482e5d8b55253ee232
MuhammedZeki <muhammed.codes@gmail.com>
2025-11-16 18:31:03 +0300

    getUser fn added

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 
-import authRoutes from "./routes/auth.routes.js"
+import authRoutes from "./routes/Auth.routes.js"
+import userRoutes from "./routes/user.routes.js"
 import connectMongoDB from "./db/mongoDb.js";
 
 dotenv.config();
@@ -16,6 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(5000, () => {
     console.log(`Server is running on port ${PORT}`);
