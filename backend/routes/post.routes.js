9835ed5f9b25677a8694f823df1018d4158e120c
MuhammedZeki <muhammed.codes@gmail.com>
2025-11-16 20:05:29 +0300

    comment fn added

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createPost, deletePost } from "../controllers/post.controller.js";
+import { commentOnPost, createPost, deletePost } from "../controllers/post.controller.js";
 import { protectRoute } from "../middlewares/protectRoute.js";
 const router = express.Router();
 
 router.post("/create", protectRoute, createPost);
 router.delete("/:id", protectRoute, deletePost);
+router.post("/comment/:id", protectRoute, commentOnPost);
 
 
 export default router;
