6624fe9ccbac183112c2a33b68d16c1f74cb0891
MuhammedZeki <muhammed.codes@gmail.com>
2025-11-16 20:11:25 +0300

    likeUnlike fn added

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { commentOnPost, createPost, deletePost } from "../controllers/post.controller.js";
+import { commentOnPost, createPost, deletePost, likeUnlikePost } from "../controllers/post.controller.js";
 import { protectRoute } from "../middlewares/protectRoute.js";
 const router = express.Router();
 
 router.post("/create", protectRoute, createPost);
 router.delete("/:id", protectRoute, deletePost);
 router.post("/comment/:id", protectRoute, commentOnPost);
+router.post("/like/:id", protectRoute, likeUnlikePost);
 
 
 export default router;
