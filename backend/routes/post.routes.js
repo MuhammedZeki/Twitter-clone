69db4cc887bd31ae56cfba5ed9c894bcd8efc2af
MuhammedZeki <muhammed.codes@gmail.com>
2025-11-17 12:29:24 +0300

    getAllPost fn added

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { commentOnPost, createPost, deletePost, likeUnlikePost } from "../controllers/post.controller.js";
+import { commentOnPost, createPost, deletePost, getAllPosts, likeUnlikePost } from "../controllers/post.controller.js";
 import { protectRoute } from "../middlewares/protectRoute.js";
 const router = express.Router();
 
@@ -7,6 +7,7 @@ router.post("/create", protectRoute, createPost);
 router.delete("/:id", protectRoute, deletePost);
 router.post("/comment/:id", protectRoute, commentOnPost);
 router.post("/like/:id", protectRoute, likeUnlikePost);
+router.get("/all", protectRoute, getAllPosts);
 
 
 export default router;
