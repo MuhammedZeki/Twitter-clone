b3fae5eaf4772841bcf6d94cf228357521826b8a
MuhammedZeki <muhammed.codes@gmail.com>
2025-11-17 13:06:06 +0300

    getUserPostList fn added

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { commentOnPost, createPost, deletePost, getAllPosts, getLikedPosts, likeUnlikePost } from "../controllers/post.controller.js";
+import { commentOnPost, createPost, deletePost, getAllPosts, getFollowingPosts, getLikedPosts, getUserPosts, likeUnlikePost } from "../controllers/post.controller.js";
 import { protectRoute } from "../middlewares/protectRoute.js";
 const router = express.Router();
 
@@ -9,6 +9,9 @@ router.post("/comment/:id", protectRoute, commentOnPost);
 router.post("/like/:id", protectRoute, likeUnlikePost);
 router.get("/all", protectRoute, getAllPosts);
 router.get("/likes/:id", protectRoute, getLikedPosts);
+router.get("/following", protectRoute, getFollowingPosts);
+router.get("/user/:username", protectRoute, getUserPosts);
+
 
 
 
