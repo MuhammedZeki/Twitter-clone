716131ea71ed65408e8adc360ec6bdc0e842f481
MuhammedZeki <muhammed.codes@gmail.com>
2025-11-19 17:34:20 +0300

    fixed

--- backend/server.js
+++ backend/server.js
@@ -35,12 +35,12 @@ app.use("/api/notifications", notificationRoutes);
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-    app.get("*", (req, res) => {
+    app.get("/*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
     });
 }
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     connectMongoDB();
 })
