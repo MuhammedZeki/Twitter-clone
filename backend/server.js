0d489a9c5e41f2d1b383df26433e7de607c4f001
MuhammedZeki <muhammed.codes@gmail.com>
2025-11-19 17:20:30 +0300

    deployment

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
 
 
-import authRoutes from "./routes/Auth.routes.js"
+import authRoutes from "./routes/auth.routes.js"
 import userRoutes from "./routes/user.routes.js"
 import postRoutes from "./routes/post.routes.js"
 import notificationRoutes from "./routes/notification.routes.js"
